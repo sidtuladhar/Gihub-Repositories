5682f89f3c95410c1cdaacd85b2ffc7a91049bbd
sidtuladhar <smt9598@nyu.edu>
2023-12-11 20:48:36 -0500

    Combine filters

--- app.py
+++ app.py
@@ -26,11 +26,15 @@ if __name__ == '__main__':
 
     st.header('Popularity of the Top 5 Most Used Programming Languages Over Time')
     top_5_lang = repo['primary_language'].value_counts().head(5).index
-    top_5_lang_df = repo[repo['primary_language'].isin(top_5_lang)]
-    top_5_lang_df['year'] = top_5_lang_df['created_at'].str[:4]
-    top_5_lang_df = top_5_lang_df[top_5_lang_df['year'] != '2023']  # 2023 not included because not enough data
-    top_5_lang_df = top_5_lang_df.groupby(['year', 'primary_language']).size().reset_index(name='count')
-    top_5_lang_df = top_5_lang_df.pivot(index='year', columns='primary_language', values='count')
+    top_5_lang_df = (
+        repo.loc[repo['primary_language'].isin(top_5_lang)].copy()
+        .assign(year=repo['created_at'].str[:4])
+        .query('year != "2023"')
+        .groupby(['year', 'primary_language'])
+        .size()
+        .reset_index(name='count')
+        .pivot(index='year', columns='primary_language', values='count')
+    )
     st.line_chart(top_5_lang_df)
 
     st.header('Top 10 Most Starred Repositories')
@@ -50,22 +54,25 @@ if __name__ == '__main__':
 
     st.header('Popularity of the Top 5 Most Used Licenses')
     top_5_licenses = repo['licence'].value_counts().head(5).index
-    top_5_licenses_df = repo[repo['licence'].isin(top_5_licenses)]
-    top_5_licenses_df['year'] = top_5_licenses_df['created_at'].str[:4]
-    top_5_licenses_df = top_5_licenses_df[top_5_licenses_df['year'] != '2023']
-    top_5_licenses_df = top_5_licenses_df.groupby(['year', 'licence']).size().reset_index(name='count')
-    top_5_licenses_df = top_5_licenses_df.pivot(index='year', columns='licence', values='count')
+    top_5_licenses_df = (
+        repo.loc[repo['licence'].isin(top_5_licenses)].copy()
+        .assign(year=repo['created_at'].str[:4])
+        .query('year != "2023"')
+        .groupby(['year', 'licence'])
+        .size()
+        .reset_index(name='count')
+        .pivot(index='year', columns='licence', values='count')
+    )
     st.line_chart(top_5_licenses_df)
 
     tab1, tab2 = st.tabs(['Yearly', 'Monthly'])
     with tab1:
         st.header('Number of Repositories Made in Each Year')
-        repo['year'] = repo['created_at'].str[:4]
+        repo.loc[:, 'year'] = repo['created_at'].str[:4]
         year_data = repo['year'].value_counts().sort_index()
-        year_data = year_data[year_data.index != '2023']
         st.bar_chart(year_data, color='#ff9a47')
     with tab2:
         st.header("Number of Repositories Made in Each Month")
-        repo['month'] = repo['created_at'].str[5:7]
+        repo.loc[:, 'month'] = repo['created_at'].str[4:7]
         month_data = repo['month'].value_counts().sort_index()
         st.bar_chart(month_data, color='#ff4747')
